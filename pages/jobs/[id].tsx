cc7fbe7ca512d36e6c38b42d8bef78c3ddef3237
Anastasiia-Basova <anastasia.basova17@gmail.com>
2022-11-08 00:21:33 +0200

    Render job details using API

--- pages/jobs/[id].tsx
+++ pages/jobs/[id].tsx
@@ -1,13 +1,27 @@
 import {useRouter} from 'next/router'
 
-const Job = () => {
+const Job = ({jobs}: any) => {
     const router = useRouter()
     const {id} = router.query
+
+    const job = jobs.find((job: any) => job.id === id)
+
     return (
         <div>
-            Job: {id}
+            {job.description}
         </div>
     )
 }
 
 export default Job
+
+export async function getServerSideProps() {
+    const res = await fetch('https://api.json-generator.com/templates/ZM1r0eic3XEy/data',
+        {headers: {Authorization: "Bearer wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu"}})
+    const jobs = await res.json();
+    return {
+        props: {
+            jobs
+        }
+    }
+}
